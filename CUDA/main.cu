f5c449959bda1f49b6150de84b49e459be401f8d
MariusValentinDirvareanu <dirvareanumarius@live.com>
2020-05-26 18:09:02 +0300

    Exercitii CUDA

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -1,42 +1,46 @@
-#include <stdio.h>
+#include <iostream>
+#include <math.h>
 
+// Kernel function to add the elements of two arrays
 __global__
-void saxpy(int n, float a, float *x, float *y)
+void add(int n, float *x, float *y)
 {
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i < n) y[i] = a*x[i] + y[i];
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
 {
   int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+  float *x, *y;
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
 
+  // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
 
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+  // Run kernel on 1M elements on the GPU
+  add<<<1, 1>>>(N, x, y);
 
-  // Perform SAXPY on 1M elements
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
 
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
-
+  // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
-    maxError = max(maxError, abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
 
-  cudaFree(d_x);
-  cudaFree(d_y);
-  free(x);
-  free(y);
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  
+  return 0;
 }
