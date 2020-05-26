7c43885068b1a5097a325134711147c965ffd59d
MariusValentinDirvareanu <dirvareanumarius@live.com>
2020-05-26 15:30:29 +0300

    Modificari mesaj AES128

--- C++/AES128.cpp
+++ C++/AES128.cpp
@@ -248,7 +248,7 @@ void PrintHex(unsigned char x)
 
 int main()
 {
-	unsigned char mesaj[] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+	unsigned char mesaj[] = "For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, but three different key lengths: 128, 192 and 256 bits.";
 	unsigned char cheie[17] = "u43x2l6gjng24edf";
 	cout << mesaj << '\n';
 	int lungimeMesajOriginala = (int)(strlen((const char *)mesaj));
